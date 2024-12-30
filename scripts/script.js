87f4b8ff34ea300839b6eb1956be96b21232514e
Sebastián Pierini <seba.i.pierini@gmail.com>
2024-12-30 17:09:06 -0300

    se agrega página de contacto

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+fetch('peliculas.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach(element => {
+    const div = document.getElementById('galeria-id');
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    a.href = "Reseñas.html";
+    img.src = element.img;
+    img.class = "box";
+    a.appendChild(img);
+    div.appendChild(a);
+    });
+})
+.catch(error => {
+    console.error('Error al obtener los datos:', error);
+});
