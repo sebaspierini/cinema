4d1fe8392ecf8b20a0e6898e47eb0187b1956647
Sebastián Pierini <seba.i.pierini@gmail.com>
2024-12-30 17:42:37 -0300

    se agrega un poco de funcionalidad sobre el click de cada pelicula para que te lleve a la reseña correspondiente

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,14 @@ fetch('peliculas.json')
 .then(response => response.json())
 .then(data => {
     data.forEach(element => {
-    const div = document.getElementById('galeria-id');
-    const a = document.createElement("a");
-    const img = document.createElement("img");
-    a.href = "Reseñas.html";
-    img.src = element.img;
-    img.class = "box";
-    a.appendChild(img);
-    div.appendChild(a);
+        const div = document.getElementById('galeria-id');
+        const a = document.createElement("a");
+        const img = document.createElement("img");
+        a.href = "Reseñas.html?id="+element.id;
+        img.src = element.img;
+        img.class = "box";
+        a.appendChild(img);
+        div.appendChild(a);
     });
 })
 .catch(error => {
